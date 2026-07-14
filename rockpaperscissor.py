fbd7d481936e8a50432bfe2f429f54d23d456007
Riya160305 <riyasain303@gmail.com>
2026-07-14 19:04:12 +0530

    rock, paper, scissors Game

---
+++ rockpaperscissor.py
@@ -0,0 +1,23 @@
+import random
+print("Rock" , "Paper" , "Scissors Game")
+options = ["rock" , "paper" , "scissors"]
+user = input("Enter your choice : ").lower()
+computer = random.choice(options)
+print("computer : " , computer)
+if user == computer:
+    print("it is a Tie")
+elif user == "rock" and computer == "scisssors":
+    print("You Win")
+elif user == "paper" and computer == "rock":
+    print("You Win")
+elif user == "scissors" and computer == "paper":
+    print("You Win")
+elif user == "scissors" and computer == "rock":
+    print("Computer Win")
+elif user == "rock" and computer == "paper":
+    print("Computer Win")
+elif user == "paper" and computer == "scissors":
+    print("Computer Win")
+else:
+    print("Invalid input")
+   
